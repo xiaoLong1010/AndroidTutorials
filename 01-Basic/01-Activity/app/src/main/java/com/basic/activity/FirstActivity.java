47a65e17646af6eb124b330b0805ab79291fe2ee
chenlong11 <chenlong11@baidu.com>
2018-09-03 20:47:27 +0800

    01-Activity, Create a layout file

--- 01-Basic/01-Activity/app/src/main/java/com/basic/activity/FirstActivity.java
+++ 01-Basic/01-Activity/app/src/main/java/com/basic/activity/FirstActivity.java
@@ -8,5 +8,7 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.first_layout);
+        
     }
 }
